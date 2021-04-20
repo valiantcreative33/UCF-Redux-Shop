8ab38f13a717f2139b8a4ac836d69d8e8a19ad3b
Ruben Matamoros <ruben@valiantcreative.net>
2021-04-19 23:14:30 -0400

    added the MONGODB connection and seeded it for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://valiantcreative33:doC4life33@cluster0.sjxzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
